8e2d207b4fcbde3284beb7a1ca1cbb2b4c0c95e6
Josh Weinstock <josh82093@gmail.com>
2019-12-26 15:15:39 -0800

    updating invitation script

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzHnoVHTajfe834Fh972vVeLNmoVnL_KmFHbMOEjQ/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyQurKYFxXVgZQBFoNfDQ7HxnuHbK8T5-S2UqTQBw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
